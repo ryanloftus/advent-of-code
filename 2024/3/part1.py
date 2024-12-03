bb2589b2ae18f11f9a36080469957745291387db
Ryan Loftus <ryanloftus02@outlook.com>
2024-12-03 01:07:22 -0500

    splitting day 3

--- 2024/3/part1.py
+++ 2024/3/part1.py
@@ -3,19 +3,13 @@ import re
 with open("input.txt") as f:
     program = f.read()
 
-pattern = "(mul\([0-9]{1,3},[0-9]{1,3}\))|(do\(\))|(don't\(\))"
+pattern = "mul\([0-9]{1,3},[0-9]{1,3}\)"
 
 matches = re.findall(pattern, program)
 
 total = 0
-do = True
-for mult_match, do_match, dont_match in matches:
-    if do_match:
-        do = True
-    elif dont_match:
-        do = False
-    elif do:
-        nums = mult_match[4:-1].split(",")
-        total += int(nums[0]) * int(nums[1])
+for match in matches:
+    nums = match[4:-1].split(",")
+    total += int(nums[0]) * int(nums[1])
 
 print(total)
